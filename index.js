4aa48d7724500218a262c95948894272d6fdf683
Michael Yiu <maplepie32@gmail.com>
2022-10-09 14:32:32 -0700

    created inquirer prompts for the methods in Employee, engineer, manager, and engineer

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-const inquirer = require('inquirer');
-const engineer = require('./lib/Engineer');
-const intern = require('./lib/Intern');
-const manager = require('./lib/Manager');
-const employee = require('./lib/Employee');
+
 const Manager = require('./lib/Manager');
 
 const newManager = new Manager();
